6ab3e01d3a57d0a8466e2e956af802e36a009532
yarinab1 <57402773+yarinab1@users.noreply.github.com>
2021-11-10 21:20:10 +0200

    Add files via upload

---
+++ components/Inputs.js
@@ -0,0 +1,56 @@
+import React,{Component} from 'react';
+import { StyleSheet,View,TouchableOpacity } from 'react-native';
+import { Input } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+class Inputs extends Component {
+    state = {isFocused: false};
+
+    onFocusChange = () => {
+        this.setState({isFocused:true})
+    }
+
+    render() {
+        return(
+            <View style={[styles.container,{borderColor:this.stateisFocused ? 
+            '#0779ef' : '#eee'}]}>
+                <Input 
+                    placeholder={this.props.name}
+                    onFocus={this.onFocusChange}
+                    inputContainerStyle={styles.inputContainer}
+                    inputStyle={styles.inputText}
+                    secureTextEntry={this.props.pass}
+                    leftIcon={
+                        <Icon 
+                            name={this.props.icon}
+                            size={22}
+                            color={this.state.isFocused ? '#0779e4' : 'grey'}    
+                        />
+
+                    }
+                />
+            </View>
+        );
+    };
+};
+
+const styles = StyleSheet.create({
+    container: {
+        width:'90%' ,
+        height: 50,
+        borderRadius: 100,
+        marginVertical:10,
+        borderWidth:3.5
+    },
+    inputContainer: {
+        borderBottomWidth:0
+    },
+    inputText: {
+        color: '#0779e4',
+        fontWeight: 'bold',
+        marginLeft:5
+    }
+
+});
+
+export default Inputs;

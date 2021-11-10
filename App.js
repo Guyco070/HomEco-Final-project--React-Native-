82f47d052a28f5dc497d62119553a78b6691b3e6
yarinab1 <yarinab1@ac.sce.ac.il>
2021-11-10 21:13:04 +0200

    upload file

--- App.js
+++ App.js
@@ -2,12 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import SignUp from './screens/SignUp';
+import Login from './screens/Login';
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Login} option={{headerShown: false}}/>
+        <Stack.Screen name="SignUp" component={SignUp} option={{headerShown: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
